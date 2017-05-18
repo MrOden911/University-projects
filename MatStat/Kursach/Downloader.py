bcbe66150be5f9aaad032bb80281fafa73016848
MrOden911 <oden911@yandex.ru>
2017-05-18 13:07:38 +0300

    Оптимизация кода

--- MatStat/Kursach/Downloader.py
+++ MatStat/Kursach/Downloader.py
@@ -2,6 +2,7 @@ import csv
 from yahoo_finance import Share
 from pprint import pprint
 
+
 def get_data(tick):
     try:
         return Share(tick)
@@ -9,31 +10,43 @@ def get_data(tick):
         print('Ошибка на сервере!')
         get_data(tick)
 
+
+def download(tickers):
+    error_tick = {}
+    for tick in tickers:
+        print('\nТикер: ', tick)
+        yah = get_data(tick)
+        try:
+            yah.get_historical('2014-01-01', '2016-01-01')
+        except Exception as e:
+            print('Тикер {} пропущен.'.format(tick))
+            print('Причина: ', e)
+            error_tick.update({tick: e})
+            continue
+        with open('data/' + tick + '.csv', 'w') as csvfile:
+            write = csv.writer(csvfile)
+            for position in yah:
+                write.writerow([position['Adj_Close']])
+        csvfile.close()
+        print('Тикер {} загружен.'.format(tick))
+    return error_tick
+
+
 tickers = []
 TickFile = open('data/tickers.txt', 'r')
 for line in TickFile:
     tickers.append(line.replace('\n', ''))
 TickFile.close()
 
+while True:
+    error_tick = download(tickers)
+    if len(error_tick) == 0:
+        break
+    print('\nОшибочные тикеры:')
+    pprint(error_tick)
+    quest = input('Повторить скачивание ошибочных тикеров? (+/-) ')
+    if quest != '+':
+        break
+    tickers = list(set(tickers) & set(error_tick))
 
-error_tick = {}
-for tick in tickers:
-    print('\nТикер: ', tick)
-    yah = get_data(tick)
-    try:
-        yah.get_historical('2014-01-01', '2016-01-01')
-    except Exception as e:
-        print('Тикер {} пропущен.'.format(tick))
-        print('Причина: ', e)
-        error_tick.update({tick: e})
-        continue
-    with open('data/' + tick + '.csv', 'w') as csvfile:
-        write = csv.writer(csvfile)
-        for position in yah:
-            write.writerow([position['Adj_Close']])
-    csvfile.close()
-    print('Тикер {} загружен.'.format(tick))
-
-print('\nОшибочные тикеры:')
-pprint(error_tick)
 print('\nЗавершено')
--- odenarium/YorN.py
+++ odenarium/YorN.py
@@ -1,10 +1,13 @@
 import odenarium
 
+
 def YorN(vopr='(Yes/No)'):
-    N=('n','-','0')
-    Y=('y','+','1')
+    N = ('n', '-', '0')
+    Y = ('y', '+', '1')
     while True:
-        r=input(vopr)[0].lower()
+        r = input(vopr)
+        if r != '':
+            r[0].lower()
         if r in Y:
             return True
         elif r in N:
